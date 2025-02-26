331b44b6e452d8c90cc984caed3cd77b09abb2a3
Marcos Fernández <maez@languagewire.com>
2025-02-26 08:07:06 +0100

    Configured cloudflare pages instead of github ones

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,41 @@
-import adapter from "@sveltejs/adapter-static"
+import adapterStatic from "@sveltejs/adapter-static"
+import adapterCloudflare from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+
+let adapter
+
+switch (process.env.ADAPTER) {
+  case 'cloudflare':
+    adapter = adapterCloudflare({
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    })
+      break;
+  default:
+    adapter = adapterStatic({
+      precompress: true,
+      fallback: '404.html'
+    })
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      fallback: "404.html",
-    }),
-    paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    adapter: adapter,
+    serviceWorker: {
+      register: false,
     },
+    paths: {
+      base: '',
+      relative: false
+    },
+    csrf: {
+      checkOrigin: true
+    },
+    embedded: false
   },
 }
 
